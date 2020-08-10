fc8d964144bcbda599c388747371047667134642
KremSH <61909124+KremSH@users.noreply.github.com>
2020-08-10 16:28:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,55 +29,7 @@ return(
       <Route path="/Artist" exact component ={Artist}/>
       </Switch>
       </div>
-        {/*{this.state.Sdata.map(song=>{
-        return(
-        <li>
-        <u>Name</u>:&nbsp;&nbsp;&nbsp;{song.Name}
-        <ul>
-        <li>
-        <b>Genre</b>:&nbsp;&nbsp;&nbsp;{song.Genre}</li>
-        <li><b>Artist</b>:&nbsp;&nbsp;&nbsp;{song.Artist}</li>
-        <li><b>Album</b>:&nbsp;&nbsp;&nbsp;{song.Album}
-        </li>
-        </ul>
-        </li>
-        );
-        })}*/}
-
-        {/*{this.state.Adata.map(Artist=>{
-        return(
-        <li>
-        <u>Name</u>:&nbsp;&nbsp;&nbsp;{Artist.Name}
-        <ul>
-        <li>
-        <b>Type</b>:&nbsp;&nbsp;&nbsp;{Artist.Type}</li>
-        <li style={{width:300 , overflowWrap: "break-word"}}><b>Albums</b>:&nbsp;&nbsp;&nbsp;{Artist.Album}</li>
-        <li style={{width:300 , overflowWrap: "break-word"}}><b>Songs</b>:&nbsp;&nbsp;&nbsp;{Artist.Song}
-        </li>
-        <li><b>Date of Birth or Formation</b>:&nbsp;&nbsp;&nbsp;{Artist.DateOfFormationorBirth}
-        </li>
-        <li style={{width:300 , overflowWrap: "break-word"}}><b>Bio</b>:&nbsp;&nbsp;&nbsp;{Artist.Bio}</li>
-        </ul>
-        </li>
-        );
-        })}*/}
-
-        {/*{this.state.Aldata.map(Album=>{
-        return(
-        <li>
-        <u>Name</u>:&nbsp;&nbsp;&nbsp;{Album.Name}
-        <ul>
-        <li>
-        <b>Genre</b>:&nbsp;&nbsp;&nbsp;{Album.Genre}</li>
-        <li style={{width:300 , overflowWrap: "break-word"}}><b>Songs</b>:&nbsp;&nbsp;&nbsp;{Album.Song}</li>
-        <li><b>Artist</b>:&nbsp;&nbsp;&nbsp;{Album.Artist}
-        </li>
-        <li><b>Date Published</b>:&nbsp;&nbsp;&nbsp;{Album.DatePublihsed}
-        </li>
-        </ul>
-        </li>
-        );
-        })}*/}
+     
         <Footer />
 
     </Fragment>
